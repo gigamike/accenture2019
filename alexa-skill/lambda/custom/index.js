bc28310156855d3fd1a779f6193c2c491787a55d
gigamike <michael.gerard.galon@gmail.com>
2019-07-20 10:23:28 +0800

    alexa-skills template

---
+++ alexa-skill/lambda/custom/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+const request = require('request');
+const https = require('https');
+const await = require('await');
+const Promise = require('promise');
